f5cc6ea205e938c0eed89710b0d0b46c23769e7a
George Drak <sonicgd@gmail.com>
2021-09-10 17:31:08 +0500

    feat(php5): add php 5.6.40

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-gdpat="7.3.*"
-memcachepat="8.*.*"
+php8pattern="8.*.*"
+php5pattern="5.6.*"
+php7pattern="7.*.*"
+php73pattern="7.3.*"
 apt update && apt install -y \
     libxml2-dev \
     zlib1g-dev \
@@ -16,18 +18,21 @@ apt update && apt install -y \
     binutils \
     && ln -s /usr/include/x86_64-linux-gnu/gmp.h /usr/local/include/ \
     && docker-php-source extract \
-    && pecl bundle -d /usr/src/php/ext redis \
+    && if [[ $PHP_VERSION =~ $php5pattern ]]; then pecl bundle -d /usr/src/php/ext redis-4.0.0; else pecl bundle -d /usr/src/php/ext redis; fi \
     && docker-php-ext-install -j$(nproc) redis \
-    && pecl bundle -d /usr/src/php/ext mongodb \
-    && docker-php-ext-install -j$(nproc) mongodb \
-    && pecl bundle -d /usr/src/php/ext grpc \
-    && docker-php-ext-install -j$(nproc) grpc \
-    && pecl bundle -d /usr/src/php/ext memcached-3.1.5 \
+    && if [[ ! $PHP_VERSION =~ $php5pattern ]]; then pecl bundle -d /usr/src/php/ext mongodb; fi \
+    && if [[ ! $PHP_VERSION =~ $php5pattern ]]; then docker-php-ext-install -j$(nproc) mongodb; fi \
+    && if [[ ! $PHP_VERSION =~ $php5pattern ]]; then pecl bundle -d /usr/src/php/ext grpc; fi \
+    && if [[ ! $PHP_VERSION =~ $php5pattern ]]; then docker-php-ext-install -j$(nproc) grpc; fi \
+    && if [[ $PHP_VERSION =~ $php5pattern ]]; then pecl bundle -d /usr/src/php/ext memcached-2.2.0; else pecl bundle -d /usr/src/php/ext memcached-3.1.5; fi \
     && docker-php-ext-install -j$(nproc) memcached \
     && docker-php-ext-configure gmp \
-    && if [[ $PHP_VERSION =~ $gdpat ]]; then docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/; else docker-php-ext-configure gd --with-freetype --with-jpeg; fi \
+    && if [[ $PHP_VERSION =~ $php73pattern ]]; then docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/; else docker-php-ext-configure gd --with-freetype --with-jpeg; fi \
     && docker-php-ext-install pdo_pgsql pgsql soap zip xsl opcache pcntl gd bcmath pdo_mysql mysqli gmp exif intl fileinfo \
-    && if [[ $PHP_VERSION =~ $memcachepat ]]; then pecl bundle -d /usr/src/php/ext memcache-8.0; else pecl bundle -d /usr/src/php/ext memcache-4.0.5.2; fi \
+    && if [[ $PHP_VERSION =~ $php5pattern ]]; then docker-php-ext-install mysql; fi \
+    && if [[ $PHP_VERSION =~ $php5pattern ]]; then pecl bundle -d /usr/src/php/ext memcache-3.0.8; fi \
+    && if [[ $PHP_VERSION =~ $php7pattern ]]; then pecl bundle -d /usr/src/php/ext memcache-4.0.5.2; fi \
+    && if [[ $PHP_VERSION =~ $php8pattern ]]; then pecl bundle -d /usr/src/php/ext memcache-8.0; fi \
     && docker-php-ext-install memcache \
     && docker-php-source delete \
     && strip --strip-debug $(php-config --extension-dir)/*.so
