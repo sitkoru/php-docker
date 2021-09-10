ad7f1671059f59453dd03d601313feab22b95ad4
George Drak <sonicgd@gmail.com>
2021-09-10 18:39:49 +0500

    fix(php5): install old xdebug, don't install ast

---
+++ install_dev.sh
@@ -0,0 +1,13 @@
+php8pattern="8.*.*"
+php5pattern="5.6.*"
+php7pattern="7.*.*"
+php73pattern="7.3.*"
+apt update \
+    && apt install -y $PHPIZE_DEPS openssh-server git unzip rsync \
+    && if [[ $PHP_VERSION =~ $php5pattern ]]; then pecl bundle -d /usr/src/php/ext xdebug-2.5.5; fi \
+    && if [[ $PHP_VERSION =~ $php5pattern ]]; then docker-php-ext-install -j$(nproc) xdebug; fi \
+    && if [[ ! $PHP_VERSION =~ $php5pattern ]]; then pecl bundle -d /usr/src/php/ext xdebug ast; fi \
+    && if [[ ! $PHP_VERSION =~ $php5pattern ]]; docker-php-ext-install -j$(nproc) xdebug ast; fi \
+    && apt purge -y --auto-remove -o APT::AutoRemove::RecommendsImportant=false $PHPIZE_DEPS \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
