4f3778306fce75f7dfffc51f5b702cd9668ea6ec
George Drak <sonicgd@gmail.com>
2021-09-17 10:38:43 +0500

    fix(gd): fix configure gd for all versions < 8

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@ apt update && apt install -y \
     && if [[ $PHP_VERSION =~ $php5pattern ]]; then pecl bundle -d /usr/src/php/ext memcached-2.2.0; else pecl bundle -d /usr/src/php/ext memcached-3.1.5; fi \
     && docker-php-ext-install -j$(nproc) memcached \
     && docker-php-ext-configure gmp \
-    && if [[ $PHP_VERSION =~ $php73pattern ]]; then docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/; else docker-php-ext-configure gd --with-freetype --with-jpeg; fi \
+    && if [[ ! $PHP_VERSION =~ $php8pattern ]]; then docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/; else docker-php-ext-configure gd --with-freetype --with-jpeg; fi \
     && docker-php-ext-install pdo_pgsql pgsql soap zip xsl opcache pcntl gd bcmath pdo_mysql mysqli gmp exif intl fileinfo \
     && if [[ $PHP_VERSION =~ $php5pattern ]]; then docker-php-ext-install mysql; fi \
     && if [[ $PHP_VERSION =~ $php5pattern ]]; then pecl bundle -d /usr/src/php/ext memcache-3.0.8; fi \
