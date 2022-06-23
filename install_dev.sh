051cbb406c80a06606768ea1657adca8d5195be9
George Drak <sonicgd@gmail.com>
2022-06-23 14:28:26 +0500

    fix: install xdebug 2.9.8 to 7.1

--- install_dev.sh
+++ install_dev.sh
@@ -6,9 +6,11 @@ apt update \
     && apt install -y $PHPIZE_DEPS openssh-server git unzip rsync \
     && if [[ $PHP_VERSION =~ $php5pattern ]]; then pecl bundle -d /usr/src/php/ext xdebug-2.5.5; fi \
     && if [[ $PHP_VERSION =~ $php5pattern ]]; then docker-php-ext-install -j$(nproc) xdebug; fi \
-    && if [[ ! $PHP_VERSION =~ $php5pattern ]]; then pecl bundle -d /usr/src/php/ext xdebug; fi \
-    && if [[ ! $PHP_VERSION =~ $php5pattern ]]; then pecl bundle -d /usr/src/php/ext ast; fi \
-    && if [[ ! $PHP_VERSION =~ $php5pattern ]]; then docker-php-ext-install -j$(nproc) xdebug ast; fi \
+    && if [[ $PHP_VERSION =~ $php71pattern ]]; then pecl bundle -d /usr/src/php/ext xdebug-2.9.8; fi \
+    && if [[ $PHP_VERSION =~ $php71pattern ]]; then docker-php-ext-install -j$(nproc) xdebug; fi \
+    && if [[ ! $PHP_VERSION =~ $php5pattern ]] && [[ ! $PHP_VERSION =~ $php71pattern ]]; then pecl bundle -d /usr/src/php/ext xdebug; fi \
+    && if [[ ! $PHP_VERSION =~ $php5pattern ]] && [[ ! $PHP_VERSION =~ $php71pattern ]]; then pecl bundle -d /usr/src/php/ext ast; fi \
+    && if [[ ! $PHP_VERSION =~ $php5pattern ]] && [[ ! $PHP_VERSION =~ $php71pattern ]]; then docker-php-ext-install -j$(nproc) xdebug ast; fi \
     && apt purge -y --auto-remove -o APT::AutoRemove::RecommendsImportant=false $PHPIZE_DEPS \
     && apt clean \
     && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
